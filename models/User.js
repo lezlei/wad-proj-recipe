cb7fdb1703c6a44545d69b3a65b0d2339a0ccb22
Tyler <57292651+lezlei@users.noreply.github.com>
2026-03-08 16:14:08 +0800

    Setup: server.js boilerplate and User model
    Added express-session for auth, connected to Atlas, and created User schema with 5 required fields.

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    // For authorisation
+    role: { 
+        type: String, 
+        default: 'user' 
+    },
+    dateJoined: { 
+        type: Date, 
+        default: Date.now 
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
- const express = require("express");
+const express = require("express");
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
+const session = require("express-session"); 
+const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+
+app.use(session({
+  secret: "smu-is113-secret-key",
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.get("/", (req, res) => {
-    res.send("Recipe App Running");
+    res.send("Recipe App Running.");
 });
 
-mongoose.connect("mongodb://127.0.0.1:27017/recipeApp")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+const dbURI = "mongodb+srv://db_user:wadrecipe@wad-recipe-proj.xa42ecg.mongodb.net/WAD-recipe-proj?appName=WAD-recipe-proj";
+
+mongoose.connect(dbURI)
+  .then(() => console.log("✅ MongoDB Connected to Atlas"))
+  .catch(err => console.log("❌ DB Connection Error:", err));
 
 app.listen(8000, () => {
-    console.log("Server running on port 8000");
+    console.log("🚀 Server running on port 8000");
 });
