d9b8ee577bc309c6c7a6fae3739f19ce656e4270
LuoChuan91 <lc.seow.2025@computing.smu.edu.sg>
2026-03-20 12:15:21 +0800

    added some minor comments

--- routes/recipes.js
+++ routes/recipes.js
@@ -11,8 +11,10 @@ router.get('/recipes/create', recipeController.createGet);
 // Post Form after filling in the recipe form
 router.post('/recipes/create', recipeController.createPost);
 
+// Get Form to edit recipe
 router.get('/recipes/:id/edit',recipeController.updateGet)
 
+// Post Form to update recipe
 router.post('/recipes/:id/update',recipeController.updatePost)
 
 // Post Form to delete recipe
