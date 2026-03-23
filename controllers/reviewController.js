740ec821c2daab6781f20c6b8b142a27757cd5b7
adizcvdj <athithya.sp.2025@smu.edu.sg>
2026-03-23 13:44:42 +0800

    review.js
    add explanation of logic

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -45,6 +45,7 @@ exports.updateReview = async (req, res) => {
     const userRole = req.session.role;
 
     const reviewData = await Review.findById(reviewId).populate("user");
+    // this is for mongoDB to go and fetch the review data based on the userId so we can do reviewData.recipe;
     if (!reviewData) return res.send("Review not found.");
 
     if (reviewData.user._id.toString() !== userId && userRole !== "admin") {
